b9c92693fc6e4345cb4807b990f2290abd336209
KudriavtcevRV <ruslan_kudriavtcev@mail.ru>
2019-09-22 19:54:55 +0300

    переделал функцию setTimeout

--- src/js/main.js
+++ src/js/main.js
@@ -7,11 +7,11 @@ var close = document.querySelector('.close');
 }); */
 button.addEventListener('click', function(){
   modal.classList.add('modal_active');
-
+  setTimeout(function(){
+    modal.classList.remove('modal_active');
+  }, 5000);
 });
-setInterval(function(){
-  modal.classList.remove('modal_active');
-},5000);
+
 
 close.addEventListener('click', function(){
   modal.classList.remove('modal_active');
