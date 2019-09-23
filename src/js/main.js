59df866d3d6bd9b7b1fae18b19db20461fb32a9d
KudriavtcevRV <ruslan_kudriavtcev@mail.ru>
2019-09-23 23:55:25 +0300

    Сделал вызов модального окна  на Jquery и кнопку вверх

--- src/js/main.js
+++ src/js/main.js
@@ -1,18 +1,49 @@
-var button = document.querySelector('#button');
+/* var button = document.querySelector('#button');
 var modal = document.querySelector('#modal');
 var close = document.querySelector('.close');
 
-/* button.addEventListener('click', function(){
-  modal.classList.add('modal_active');
-}); */
 button.addEventListener('click', function(){
   modal.classList.add('modal_active');
-  setTimeout(function(){
+});
+close.addEventListener('click', function(){
+  // при нажатии на крестик, модальное окно закрывается
+  modal.classList.remove('modal_active');
+});
+/* button.addEventListener('click', function(){
+// при нажатии на кнопку появляется модальное окно
+  modal.classList.add('modal_active');
+// модальное окно закрывается через 5сек
+  window.setTimeout(function(){
     modal.classList.remove('modal_active');
   }, 5000);
 });
+ */
+$(document).ready(function(){
+  var button = $('#button');
+  var modal = $('#modal');
+  var close = $('.close');
 
-
-close.addEventListener('click', function(){
-  modal.classList.remove('modal_active');
+  button.on('click', function(){
+    modal.addClass('modal_active');
+  });
+  close.on('click', function(){
+    modal.removeClass('modal_active');
+  });
 });
+
+$(document).ready(function(){
+  let backToTop = $('.btn');
+
+  backToTop.on('click', function(){
+    $('html').animate({
+      scrollTop: 0
+    }, 1500);
+  });
+});
+
+
+
+
+
+
+
